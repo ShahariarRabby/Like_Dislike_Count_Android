4f254face9a40d8b70353408644b57a48c9d18aa
Shahariar Rabby <shahariarrabby@gmail.com>
2017-09-26 22:48:18 +0600

    value save after rotation

--- app/src/main/java/com/rabby/counter/MainActivity.java
+++ app/src/main/java/com/rabby/counter/MainActivity.java
@@ -7,11 +7,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public final String save_value_like = "Save";
+    public final String save_value_dlike = "dSave";
     public Button likeButton, dislikeButton, resetButton;
     public TextView likeText;
     public TextView dislikeText;
-    public int like =0;
+    public int like = 0;
     public int dislike = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +53,23 @@ public class MainActivity extends AppCompatActivity {
         likeButton.setOnClickListener(ourLikeClick);
         dislikeButton.setOnClickListener(ourDislikeClick);
         resetButton.setOnClickListener(ourResetButton);
+    }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString(save_value_like, likeText.getText().toString());
+        outState.putString(save_value_dlike, dislikeText.getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String save = savedInstanceState.getString(save_value_like);
+        String save1 = savedInstanceState.getString(save_value_dlike);
+        like = Integer.parseInt(save);
+        dislike = Integer.parseInt(save1);
+        likeText.setText(save);
+        dislikeText.setText(save1);
     }
 }
